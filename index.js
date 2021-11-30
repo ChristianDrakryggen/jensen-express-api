b18ea4ff147ca6339ffe2ce1eccd9901e329ef3e
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-11-30 15:45:40 +0100

    endpoint added

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const router = require("./api/API");
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/api", router);
 
 mongoose.connect(
   "mongodb+srv://christianDrakryggenJensen:i7fc2WP02HtIHHFu@my-first-api-db.3kdti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true },
   () => console.log("Connected to db")
 );
+
+/*app.get("/posts", (req, res) => {
+  res.send("Hello");
+});*/
 
 const PORT = process.env.PORT || 5000;
 
