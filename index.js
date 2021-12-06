84f06f596dc40b76c65eafa2bf9bc78044a971ff
Christian Johannesson <christian.johannesson@drakryggen.com>
2021-12-06 11:03:41 +0100

    client folder added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const router = require("./api/API");
+const path = require("path");
 
 app.use(cors());
 
@@ -10,8 +11,10 @@ app.use(express.json());
 
 app.use("/api", router);
 
+app.use(express.static(path.join(__dirname, "client")));
+
 mongoose.connect(
-  "mongodb+srv://christianDrakryggenJensen:i7fc2WP02HtIHHFu@my-first-api-db.3kdti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  /*"mongodb+srv://christianDrakryggenJensen:i7fc2WP02HtIHHFu@my-first-api-db.3kdti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"*/ "mongodb://localhost:27017/posts-db",
   { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true },
   () => console.log("Connected to db")
 );
